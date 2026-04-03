809dfb6ca757b9c4a2a01c5132060f9bf565c8f2
devopshubpro-collab <devopshubpro@gmail.com>
2026-04-03 14:50:19 +0530

    Add AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY variable definitions

--- variables.tf
+++ variables.tf
@@ -1,35 +1,9 @@
-variable "aws_region" {
-  description = "AWS region for resources"
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key ID"
   type        = string
-  default     = "us-east-1"
 }
 
-variable "instance_type" {
-  description = "EC2 instance type"
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret access key"
   type        = string
-  default     = "t2.micro"
-}
-
-variable "instance_name" {
-  description = "Name tag for the EC2 instance"
-  type        = string
-  default     = "terraform-instance"
-}
-
-variable "enable_monitoring" {
-  description = "Enable detailed monitoring for EC2 instance"
-  type        = bool
-  default     = true
-}
-
-variable "root_volume_size" {
-  description = "Size of the root volume in GB"
-  type        = number
-  default     = 20
-}
-
-variable "enable_public_ip" {
-  description = "Enable public IP assignment"
-  type        = bool
-  default     = true
 }
