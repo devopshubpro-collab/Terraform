eac682eaa2c329d69dbd23f8060177f967528ae7
devopshubpro-collab <devopshubpro@gmail.com>
2026-04-03 14:30:29 +0530

    Update provider.tf with AWS provider details

--- provider.tf
+++ provider.tf
@@ -10,6 +10,6 @@ terraform {
 
 provider "aws" {
   region     = var.aws_region
-  AWS_ACCESS_KEY_ID = var.AWS_ACCESS_KEY_ID
- AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
